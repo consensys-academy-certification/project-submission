bfacb63c77b1ad593b0b19e2a5d6c67de6af63c8
josh <critesjosh@gmail.com>
2019-10-31 13:02:09 -0400

    comment out old line

--- travis_scripts/setup.sh
+++ travis_scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -
 git clone https://gist.github.com/f2da4709b636dce11de911975db6c818.git
 rm ./test/ProjectSubmission.test.js
-mv ./f2da4709b636dce11de911975db6c818/certification-SupplyChain.test.js ./test/ProjectSubmission.test.js
+# mv ./f2da4709b636dce11de911975db6c818/certification-SupplyChain.test.js ./test/ProjectSubmission.test.js
 ganache-cli &
 
