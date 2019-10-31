248c1fdfd7800fa91888fe7628de1faa400d1fdc
josh <critesjosh@gmail.com>
2019-10-31 14:08:46 -0400

    no longer needed

--- travis_scripts/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -
-git clone https://gist.github.com/f2da4709b636dce11de911975db6c818.git
-rm ./test/ProjectSubmission.test.js
-# mv ./f2da4709b636dce11de911975db6c818/certification-SupplyChain.test.js ./test/ProjectSubmission.test.js
-ganache-cli &
-
--- travis_scripts/web3-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash -
-truffle migrate
-npm run test
